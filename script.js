0c3b4a50cab69dcdc6ee05a21870526412d23ad0
Dariya <58099281+direcase@users.noreply.github.com>
2021-07-12 16:57:04 +0600

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ window.addEventListener("scroll", function () {
     let header = document.querySelector(".nav");
     header.classList.toggle("sticky", window.scrollY > 0);
 })
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
